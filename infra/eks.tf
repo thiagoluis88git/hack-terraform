f685f841b8ba7f4f9c9533b17beca7d066552bfc
Thiago Luis <thiago.luis88@gmail.com>
2025-01-25 20:18:24 -0300

    fix: rds_config variables and cluster name

--- infra/eks.tf
+++ infra/eks.tf
@@ -17,7 +17,7 @@ resource "aws_eks_cluster" "hack-cluster" {
 # NODE GROUP
 resource "aws_eks_node_group" "node-ec2" {
   for_each        = { for node_group in var.node_groups : node_group.name => node_group }
-  cluster_name    = aws_eks_cluster.fastfood-cluster.name
+  cluster_name    = aws_eks_cluster.hack-cluster.name
   node_group_name = each.value.name
   node_role_arn   = var.networking.fiap_role
   subnet_ids      = flatten(aws_subnet.private-subnet[*].id)
@@ -40,7 +40,7 @@ resource "aws_eks_node_group" "node-ec2" {
 
 resource "aws_eks_addon" "addons" {
   for_each          = { for addon in var.addons : addon.name => addon }
-  cluster_name      = aws_eks_cluster.fastfood-cluster.id
+  cluster_name      = aws_eks_cluster.hack-cluster.id
   addon_name        = each.value.name
   addon_version     = each.value.version
   resolve_conflicts = "OVERWRITE"
