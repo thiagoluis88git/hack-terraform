de7bba00b19a4af693b71b3437c7e3c53eaae7ea
Thiago Luis <thiago.luis88@gmail.com>
2025-01-26 19:26:42 -0300

    fix: adding networking environment

--- infra/gateway/variables.tf
+++ infra/gateway/variables.tf
@@ -1,3 +1,28 @@
+variable "networking" {
+  type = object({
+    cidr_block      = string
+    region          = string
+    profile         = string
+    vpc_name        = string
+    fiap_role       = string
+    azs             = list(string)
+    public_subnets  = list(string)
+    private_subnets = list(string)
+    nat_gateways    = bool
+  })
+  default = {
+    cidr_block      = "10.0.0.0/16"
+    region          = "us-east-1"
+    profile         = "fiap-local"
+    vpc_name        = "fiap-vpc"
+    fiap_role       = "arn:aws:iam::199710252834:role/LabRole"
+    azs             = ["us-east-1a", "us-east-1b"]
+    public_subnets  = ["10.0.64.0/24", "10.0.96.0/24"]
+    private_subnets = ["10.0.0.0/24", "10.0.32.0/24"]
+    nat_gateways    = true
+  }
+}
+
 variable "fastfood_aws_access_key_id" {
   description = "Load Balancer DNS"
   type        = string
